295ebe22fe26892fe3ff92fc71f0eaf06c426326
syncdecodes <syncdecodes@gmail.com>
2026-02-11 16:13:32 +0530

    Added files via git

---
+++ 01Intro/001intro.js
@@ -0,0 +1,31 @@
+const amount = 12
+if (amount < 10){
+    console.log("small number")
+}
+else{
+    console.log("large number")
+}
+console.log('hey its my first node app')
+
+/* 
+
+Node js is not a programming language its an Envirnment to run js outside browser
+built on chrome's v8 js engine 
+
+Browser     vs     Node js
+DOM                NO DOM 
+WINDOW             NO WINDOW
+INTERACTIVE APPS   SERVER SIDE APPS
+NO FILESYSTEM      FILESYSTEM
+FREAGMENTATION     VERSION
+ES6 MODULES        COMMON JS
+
+- NPM enables us to do three things -:
+reuse our own code in other projects 
+use code written by other developers 
+share our own code with other developers
+
+packages = dependencies = modules 
+there is no quality control in npm anyone can publish anything
+
+*/
---
+++ 01Intro/002globals.js
@@ -0,0 +1,19 @@
+// GLOBALS - NO WINDOWS !!
+
+// __dirname - path to current directory
+// __filename = file name
+// require - function to use modules 
+// module - info about current module
+// process - info about enviornment where the program is being executed
+
+// console.log(__dirname);
+// console.log(__filename);
+// console.log(require);
+// console.log(module);
+// console.log(process)
+
+setInterval(() => {
+    console.log("hello")
+}, 1000);
+
+// ctrl + c in terminal to stop the execution
